b1d6c785b821d7a7b6a0937054b9e35171211e45
AsayakeX <soultakerzx@gmail.com>
2020-10-16 23:42:46 -0700

    Update:Initial Board

--- src/brickBreak/Gameplay.java
+++ src/brickBreak/Gameplay.java
@@ -1,5 +1,111 @@
-package brickBreaker;
+package brickBreak;
 
-public class Gameplay{
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+
+import javax.swing.JPanel;
+
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener{
+	private boolean play = false;
+	private int score = 0;
+
+	private int totalBricks = 21;
+
+	private Timer time;
+	private int delay = 8;
+
+	private int playerX = 310;
+
+	private int ballPosX = 120;
+	private int ballPosY = 350;
+	private int ballXDir = -1;
+	private int ballYDir = -2;
+
+	public Gameplay(){
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		//timer = new Timer(delay, this);
+		//timer.start();
+	}
+
+	public void paint(Graphics g){
+		//background
+		g.setColor(Color.black);
+		g.fillRect(1, 1, 692, 592);
+
+
+		//borders
+		g.setColor(Color.yellow);
+		g.fillRect(0, 0, 3, 592);
+		g.fillRect(0, 0, 692, 3);
+		g.fillRect(672, 0, 3, 592);
+
+		//paddle
+		g.setColor(Color.green);
+		g.fillRect(playerX, 550, 100, 8);
+
+		//ball
+		g.setColor(Color.yellow);
+		g.fillOval(ballPosX, ballPosY, 20, 20);
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e){
+		//TODO
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e){
+		//Todo
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e){
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			if(playerX >= 600){
+				playerX = 600;
+			}
+			else{
+				moveRight();
+			}
+
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			if(playerX < 10){
+				playerX = 10;
+			}
+			else{
+				moveLeft();
+			}
+		}
+
+	}
+
+	public void moveRight(){
+		play = true;
+		playerX+=20;
+	}
+
+	public void moveLeft(){
+		play = true;
+		playerX-=20;
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e){
+
+	}
 	
 }
